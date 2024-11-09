c8538babcdcc00406e410d42b065060a2cd952f3
John Michael Coronel <139611817+kaffw@users.noreply.github.com>
2024-11-09 11:48:14 +0800

    ADD (Game Animation)
    added Aurora Idle (4 CD)

--- Nature's Last Plea/Assets/Scripts/PlayerController.cs
+++ Nature's Last Plea/Assets/Scripts/PlayerController.cs
@@ -14,9 +14,15 @@ public class PlayerController : MonoBehaviour
     public bool inAction = false;
 
     public float moveSpeed = 1f;
+
+    //Animation
+    private Animator anim;
+    private Vector2 lastDirection;
+    //
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
         mainCamera = GameObject.Find("Main Camera");
     }
@@ -34,6 +40,55 @@ public class PlayerController : MonoBehaviour
                 PlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
                 rb.velocity = PlayerInput * moveSpeed;
             }
+
+            if (PlayerInput != Vector2.zero)
+            {
+                if (PlayerInput.y > 0) lastDirection = Vector2.up;
+                else if (PlayerInput.y < 0) lastDirection = Vector2.down;
+                else if (PlayerInput.x > 0) lastDirection = Vector2.right;
+                else if (PlayerInput.x < 0) lastDirection = Vector2.left;
+
+                anim.SetBool("isMoving", false);
+                anim.SetBool("Idle_Front", false);
+                anim.SetBool("Idle_Back", false);
+                anim.SetBool("Idle_Left", false);
+                anim.SetBool("Idle_Right", false);
+
+                if (lastDirection == Vector2.up) anim.SetBool("Idle_Back", true);
+                else if (lastDirection == Vector2.down) anim.SetBool("Idle_Front", true);
+                else if (lastDirection == Vector2.left) anim.SetBool("Idle_Left", true);
+                else if (lastDirection == Vector2.right) anim.SetBool("Idle_Right", true);
+            }
+            //else
+            //{
+            //    anim.SetBool("isMoving", false);
+            //    anim.SetBool("Idle_Front", false);
+            //    anim.SetBool("Idle_Back", false);
+            //    anim.SetBool("Idle_Left", false);
+            //    anim.SetBool("Idle_Right", false);
+
+            //    if (lastDirection == Vector2.up) anim.SetBool("Idle_Back", true);
+            //    else if (lastDirection == Vector2.down) anim.SetBool("Idle_Front", true);
+            //    else if (lastDirection == Vector2.left) anim.SetBool("Idle_Left", true);
+            //    else if (lastDirection == Vector2.right) anim.SetBool("Idle_Right", true);
+            //}
+
+
+            //if (PlayerInput != Vector2.zero)
+            //{
+            //    lastDirection = PlayerInput;
+            //    //animator.SetBool("isMoving", true);
+            //    //animator.SetFloat("MoveX", PlayerInput.x);
+            //    //animator.SetFloat("MoveY", PlayerInput.y);
+            //}
+            //else
+            //{
+            //    //animator.SetBool("isMoving", false);
+
+            //    // Set idle animation based on last direction
+            //    //anim.SetFloat("IdleX", lastDirection.x);
+            //    //anim.SetFloat("IdleY", lastDirection.y);
+            //}
 
             //enable hitbox
             gameObject.tag = "Player";
@@ -72,4 +127,11 @@ before:
 after:
     if (Input.GetKeyDown(KeyCode.E) && !inAction)
 
+ */
+
+/*
+if (PlayerInput.y < 0) anim.SetBool("Idle_Front", true); else anim.SetBool("Idle_Front", false);
+if (PlayerInput.x < 0) anim.SetBool("Idle_Left", true); else anim.SetBool("Idle_Left", false);
+if (PlayerInput.y > 0) anim.SetBool("Idle_Back", true); else anim.SetBool("Idle_Back", false);
+if (PlayerInput.x > 0) anim.SetBool("Idle_Right", true); else anim.SetBool("Idle_Right", false);
  */
