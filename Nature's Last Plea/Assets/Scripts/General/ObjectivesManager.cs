2a9ca7d72f69648726065800c918a8e6ca7b64e0
John Michael Coronel <139611817+kaffw@users.noreply.github.com>
2024-12-03 17:11:50 +0800

    BUGFIX (Objective Spawn Manager)
    Fixed the problem where objectives can spawn on top of each other.

--- Nature's Last Plea/Assets/Scripts/General/ObjectivesManager.cs
+++ Nature's Last Plea/Assets/Scripts/General/ObjectivesManager.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class ObjectivesManager : MonoBehaviour
 {
     public GameObject[] objectives;
-    //public GameObject[] objectivesInList;
 
     public Transform[] spawnLocations;
 
     void Start()
     {
+        List<int> availableIndices = new List<int>();
+        for (int i = 0; i < spawnLocations.Length; i++)
+        {
+            availableIndices.Add(i);
+        }
+
         for (int i = 0; i < objectives.Length; i++)
         {
-            int rand = Random.Range(0, spawnLocations.Length);
-            objectives[i].transform.position = spawnLocations[rand].position;
+            if (availableIndices.Count == 0)
+            {
+                Debug.LogWarning("Not enough spawn locations for all objectives.");
+                break;
+            }
+
+            int randIndex = Random.Range(0, availableIndices.Count);
+            int spawnIndex = availableIndices[randIndex];
+
+            objectives[i].transform.position = spawnLocations[spawnIndex].position;
+
+            availableIndices.RemoveAt(randIndex);
         }
     }
 
@@ -23,5 +38,3 @@ public class ObjectivesManager : MonoBehaviour
         Debug.Log(objectives[index].transform.position);
     }
 }
-
-//objectives[i].transform.position = new Vector2(i + 2, i + 2);
